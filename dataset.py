800d4c4470daf3a0f6a6c013987b2285dc47b11f
Scott Ward <wardsj@hotmail.com>
2024-11-08 08:47:57 -0800

    some updates

--- dataset.py
+++ dataset.py
@@ -6,49 +6,58 @@ import requests
 import tiktoken
 import numpy as np
 
-from torch import stack, from_numpy, randint
-
 from cosmosis.dataset import CDataset
 
 
 class TinyShakes(CDataset):
+    """
+    https://github.com/karpathy/nanoGPT
+    """
 
-    def __getitem__(self, i):
-        block_size = 5
-        batch_size = 3
-
-        # We recreate np.memmap every batch to avoid a memory leak, as per
-        # https://stackoverflow.com/questions/45132940/\
-        # numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
-    
-        data = np.memmap('./data/encoded.bin', dtype=np.uint16, mode='r')
-    
-        ix = randint(len(data) - block_size, (batch_size,))
+    def __getitem__(self, i):         
+        ix = np.random.randint(low=0, high=len(self.ds) - self.block_size, size=(self.batch_size,))
+        X1 = np.stack([(self.ds[i:i+self.block_size]).astype(np.int64) for i in ix])
+        X2 = np.stack([(self.ds[i+1:i+1+self.block_size]).astype(np.int64) for i in ix])
         
-        x = stack([from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
-        y = stack([from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+        _data = {'X1': X1, 'X2': X2}
+        data = {}
         
-        return x, y
-
-    def load_data(self):
+        for feature, Transforms in self.transforms.items():
+            out = _data[feature]
+            for T in Transforms:
+                out = T(out)
+            data[feature] = out
+            
+        del _data
+        return data
+                
+    def load_data(self, block_size, batch_size):
         data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
         self.enc = tiktoken.get_encoding("gpt2")
         self.ds_idx = []
+        self.block_size = block_size
+        self.batch_size = batch_size
         
-        if not os.path.exists('./data/tinyshakespeare.txt'):
-            with open('./data/tinyshakespeare.txt', 'w', encoding='utf-8') as f:
+        if not os.path.exists('./data/tinyshakes.txt'):
+            with open('./data/tinyshakes.txt', 'w', encoding='utf-8') as f:
                 f.write(requests.get(data_url).text)
-            print('tinyshakespeare.txt saved in ../gpt/data/')
+            print('tinyshakes.txt downloaded and saved in ../gpt/data/')
+        else:
+            print('tinyshakes.txt loaded from saved file in ../gpt/data/')
 
-        if not os.path.exists('./data/encoded.bin'):
-            with open('./data/tinyshakespeare.txt', 'r', encoding='utf-8') as f:
+        if not os.path.exists('./data/tinyshakes_encoded.bin'):
+            with open('./data/tinyshakes.txt', 'r', encoding='utf-8') as f:
                 data = f.read()
-                print('len(data): ', len(data))
-                
             # encode with tiktoken gpt2 bpe
             tokens = self.enc.encode_ordinary(data)
             tokens = np.array(tokens, dtype=np.uint16)
-            tokens.tofile('./data/'+'encoded.bin')
-            print('tokens created and saved in file ./data/encoded.bin')
+            tokens.tofile('./data/tinyshakes_encoded.bin')
+            print('text has been tokenized and saved in file ./data/tinyshakes_encoded.bin')
         else:
-            print('tokens loaded from file ./data/encoded.bin')
+            print('tokens loaded from file ./data/tinyskakes_encoded.bin')
+            
+        # We recreate np.memmap every batch to avoid a memory leak, as per
+        # https://stackoverflow.com/questions/45132940/\
+        # numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
+        data = np.memmap('./data/tinyshakes_encoded.bin', dtype=np.uint16, mode='r')
+        return data
