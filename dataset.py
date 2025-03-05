34a3baf9e118a6bcefc4cf04aeabf49118fed460
Scott Ward <wardsj@hotmail.com>
2025-03-05 10:16:10 -0800

    working on gpt.generate

--- dataset.py
+++ dataset.py
@@ -37,12 +37,14 @@ class TinyShakes(CDataset):
         # encode the prompt with tiktoken gpt2 bpe
         tokens = self.encoding.encode_ordinary(prompt)
         ds = np.array(tokens, dtype=np.uint16)
+        self.d_seq = ds.shape[-1]
         return ds
              
-    def load_data(self, d_seq=0, n=338035, prompt=None):
+    def load_data(self, d_seq=1, n=338035, prompt=None):
         data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
         self.encoding = tiktoken.get_encoding("gpt2")
         self.d_seq, self.n = d_seq, n
+        tiny_bin = './data/tinyshakes_stripped_encoded.bin'
 
         if prompt == None:
             if not os.path.exists('./data/tinyshakes.txt'):
@@ -52,28 +54,28 @@ class TinyShakes(CDataset):
             else:
                 print('tinyshakes.txt loaded from saved file in ../gpt/data/')
     
-            if not os.path.exists('./data/tinyshakes_encoded.bin'):
+            if not os.path.exists(tiny_bin):
                 with open('./data/tinyshakes.txt', 'r', encoding='utf-8') as f:
                     data = f.read()
-                    data = data.strip()
+                    data = data.replace('\n', ' ')
                 # encode with tiktoken gpt2 bpe
                 tokens = self.encoding.encode_ordinary(data)
                 tokens = np.array(tokens, dtype=np.uint16)
-                tokens.tofile('./data/tinyshakes_encoded.bin')
-                print('text has been tokenized and saved in file ./data/tinyshakes_encoded.bin')
+                tokens.tofile(tiny_bin)
+                print('text has been tokenized and saved in file {}'.format(tiny_bin))
             else:
-                print('tokens loaded from file ./data/tinyskakes_encoded.bin')
+                print('tokens loaded from file {}'.format(tiny_bin))
                 
-            ds = np.memmap('./data/tinyshakes_encoded.bin', dtype=np.uint16, mode='r')
-            ds_idx = list(range(len(ds)-d_seq)) # 338035
+            ds = np.memmap(tiny_bin, dtype=np.uint16, mode='r')
+            ds_idx = list(range(ds.shape[-1])) # 338035
             if n != 338035: 
-                ds_idx = list(np.random.choice(ds_idx, size=n, replace=False))
+                 ds_idx = list(np.random.choice(ds_idx, size=n, replace=False))
             ds.flush()
             self.ds_idx = ds_idx
         else:
             ds = self.prompt(prompt)
             self.ds_idx = [0]
-        
+
         print('len(self.ds_idx): ', len(self.ds_idx))
         print('data.nbytes: ', ds.nbytes)
         return ds
