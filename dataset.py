1f32cf19bb989a1bd9fc8ef8300aeaef6e27d241
Scott Ward <wardsj@hotmail.com>
2026-04-08 12:19:16 -0700

    update from sagan

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 import sys # required for relative imports in jupyter lab
 sys.path.insert(0, '../')
-
+import logging
 import os
 import requests
 import tiktoken
@@ -10,6 +10,7 @@ from sys import getsizeof
 
 from cosmosis.dataset import TDataset
 
+logger = logging.getLogger(__name__)
 
 class TinyShakes(TDataset):
     """
@@ -26,9 +27,9 @@ class TinyShakes(TDataset):
             if not os.path.exists(os.path.join(self.dir, 'tinyshakes.txt')):
                 with open(os.path.join(self.dir, 'tinyshakes.txt'), 'w', encoding='utf-8') as f:
                     f.write(requests.get(data_url).text)
-                print('tinyshakes.txt downloaded and saved in {}'.format(self.dir))
+                logger.info('TinyShakes.load_data tinyshakes.txt downloaded and saved in {}'.format(self.dir))
             else:
-                print('tinyshakes.txt loaded from saved file in {}'.format(self.dir))
+                logger.info('TinyShakes.load_data inyshakes.txt loaded from saved file in {}'.format(self.dir))
 
             if not os.path.exists(tiny_bin) or os.path.getsize(tiny_bin) == 0:
                 with open(os.path.join(self.dir, 'tinyshakes.txt'), 'r', encoding='utf-8') as f:
@@ -38,15 +39,15 @@ class TinyShakes(TDataset):
                 tokens = self.encoding.encode_ordinary(data)
                 tokens = np.array(tokens, dtype=np.uint16)
                 tokens.tofile(tiny_bin)
-                print('text has been tokenized and saved in file {}'.format(tiny_bin))
+                logger.info('TinyShakes.load_data text has been tokenized and saved in file {}'.format(tiny_bin))
             else:
-                print('tokens loaded from file {}'.format(tiny_bin))
+                logger.info('TinyShakes.load_data tokens loaded from file {}'.format(tiny_bin))
 
             try:
                 ds = np.memmap(tiny_bin, dtype=np.uint16, mode='r')
             except ValueError as e:
-                print(f"error mapping file: {e}")
-                raise RuntimeError(f"Failed to load dataset at {tiny_bin}.")
+                logger.info(f"TinyShakes.load_data error mapping file: {e}")
+                raise RuntimeError(f"TinyShakes.load_data failed to load dataset at {tiny_bin}.")
 
             ds_idx = list(range(self.n-self.d_seq)) # 338035
             self.ds_idx = ds_idx
